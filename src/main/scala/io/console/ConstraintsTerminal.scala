a8fca2d931125fbe0aa7e2002c9ea739dea4df47
Augustine Kwanashie <akwanashie@gmail.com>
2017-02-15 01:51:22 +0000

    Pull out error handling into seperate file

--- src/main/scala/io/console/ConstraintsTerminal.scala
+++ src/main/scala/io/console/ConstraintsTerminal.scala
@@ -1,21 +1,26 @@
 package io.console
 
-import org.jline.reader.LineReaderBuilder
+import org.jline.reader.{LineReaderBuilder, UserInterruptException}
 import org.jline.terminal.{Terminal, TerminalBuilder}
 
 import scala.util.Try
 
 object ConstraintsTerminal extends App {
-  val terminal: Terminal = TerminalBuilder.builder().name("Constraints Solver").build()
-  val lineReader = LineReaderBuilder.builder().terminal(terminal).build()
+  private val terminal: Terminal = TerminalBuilder.builder().name("Constraints Solver").build()
+  private val lineReader = LineReaderBuilder.builder().terminal(terminal).build()
 
   readLine("constraints-solver> ")
 
-  def readLine(prompt: String): Try[_] = {
+  private def readLine(prompt: String): Try[_] = {
     Try(lineReader.readLine(prompt))
       .map(Command.apply)
       .map(_.execute())
-      .recover({ case error => println(s"Error: ${error.getMessage}") })
+      .recover(processErrors)
       .map(_ => readLine(prompt))
+  }
+
+  private def processErrors: PartialFunction[Throwable, Unit] = {
+    case error: UserInterruptException => Exit.execute()
+    case error: Throwable => println(s"Error: ${error.getMessage}")
   }
 }
