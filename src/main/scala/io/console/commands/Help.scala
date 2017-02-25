362a95c31e43d10e3219e0c198a34538ce34d37a
Augustine Kwanashie <akwanashie@gmail.com>
2017-02-25 12:24:18 +0000

    Update help command

--- src/main/scala/io/console/commands/Help.scala
+++ src/main/scala/io/console/commands/Help.scala
@@ -5,7 +5,16 @@ import io.console.ConsoleState
 object Help extends Command {
   override val stringRep: String = "help"
   override val execute = (state: ConsoleState) => {
-    println("Available commands:")
+    println(
+      s"""Available commands:
+         |[expression]            adds a constraint to the model
+         |min/max [expression]    sets the objective function
+         |clear                   clears the model
+         |help                    displays available comments
+         |print                   prints the current model
+         |save                    saves the model to file
+         |exit                    exits the console
+       """.stripMargin)
     state
   }
 }
