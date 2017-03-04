d2affe8d3b44fcd2c91c58b81305dba6f05a1d64
Augustine Kwanashie <akwanashie@gmail.com>
2017-03-04 09:39:58 +0000

    Validate user input for export command

--- src/main/scala/io/console/commands/Export.scala
+++ src/main/scala/io/console/commands/Export.scala
@@ -5,8 +5,11 @@ import io.console.ConsoleState
 object Export extends Command {
   override val stringRep: String = "export"
   override val execute = (state: ConsoleState) => {
-    val path = state.commandString.split("[ ]+").toList(1)
-    state.model.map(_.save(path))
-    state
+    state.commandString.split("[ ]+").toList match {
+      case command :: path :: Nil =>
+        state.model.map(_.save(path))
+        state
+      case _ => throw new CommandException("Please provide a file name/path")
+    }
   }
 }
